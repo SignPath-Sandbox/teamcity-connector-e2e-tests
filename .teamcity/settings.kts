716c63fcba280c70d29cccf5607719d2538f5ae0
Saleh Ahmadi <saleh.ahmadi@signpath.io>
2026-02-04 13:43:49 +0100

    updated settings.kts to include playground connector url and use parameters

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.buildSteps.signPathSubmitSigningRequest
+import jetbrains.buildServer.configs.kotlin.ParameterDisplay
 
 version = "2024.12"
 
@@ -7,6 +9,20 @@ project {
   buildType {
     name = "Build and Sign"
     id("build_and_sign")
+
+    params {
+      select("ConnectorUrl", "https://teamcity-connector-stable.customersimulation.int.signpath.io", label = "Connector URL",
+        display = ParameterDisplay.PROMPT,
+        options = listOf(
+          "https://teamcity-connector-stable.customersimulation.int.signpath.io",
+          "https://teamcity-connector-playground.customersimulation.int.signpath.io"
+        ))
+      text("SignPath.OrganizationId", "9ff791fc-c563-44e3-ab8c-86a33c910bbe", label = "SignPath Organization ID", display = ParameterDisplay.PROMPT)
+      text("SignPath.ProjectSlug", "TeamCity_Connector_E2E_Tests", label = "SignPath Project Slug", display = ParameterDisplay.PROMPT)
+      text("SignPath.SigningPolicySlug", "test-signing", label = "SignPath Signing Policy Slug", display = ParameterDisplay.PROMPT)
+      text("SignPath.ArtifactConfigurationSlug", "initial", label = "SignPath Artifact Configuration Slug", display = ParameterDisplay.PROMPT)
+      password("SignPath.ApiToken", "credentialsJSON:4ba5812a-ffd4-41a7-829c-15adcebda622")
+    }
 
     vcs {
       root(DslContext.settingsRoot)
@@ -27,18 +43,16 @@ project {
       }
 
       // sign step
-      step {
-        type = "SignPathRunner"
-        param("connectorUrl", "https://teamcity-connector-stable.customersimulation.int.signpath.io")
-        param("organizationId", "9ff791fc-c563-44e3-ab8c-86a33c910bbe")
-        param("apiToken", "credentialsJSON:4ba5812a-ffd4-41a7-829c-15adcebda622")
-        param("projectSlug", "TeamCity_Connector_E2E_Tests")
-        param("signingPolicySlug", "test-signing")
-        param("artifactConfigurationSlug", "initial")
-
-        param("inputArtifactPath", artifactPath)
-        param("outputArtifactPath", artifactPath)
-        param("waitForCompletion", "true")
+      signPathSubmitSigningRequest {
+        connectorUrl = "%ConnectorUrl%"
+        organizationId = "%SignPath.OrganizationId%"
+        apiToken = "%SignPath.ApiToken%"
+        projectSlug = "%SignPath.ProjectSlug%"
+        signingPolicySlug = "%SignPath.SigningPolicySlug%"
+        artifactConfigurationSlug = "%SignPath.ArtifactConfigurationSlug%"
+        inputArtifactPath = artifactPath
+        outputArtifactPath = artifactPath
+        waitForCompletion = true
       }
     }
 
